9196e5bb41f461beb6daa63680562ba919950498
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-08-02 20:17:43 -0700

    upload csv, removes rest of form

--- src/aind_data_transfer_gui/server.py
+++ src/aind_data_transfer_gui/server.py
@@ -1,12 +1,14 @@
 """Starts and Runs Starlette Service"""
 import os
 
-from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 from starlette_wtf import CSRFProtectMiddleware, csrf_protect
-
-from aind_data_transfer_gui.forms import SubmitJobsForm, UploadJobForm
+from starlette.routing import Route
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import JSONResponse
+import csv
+import io
 
 SECRET_KEY = "secret key"
 CSRF_SECRET_KEY = "csrf secret key"
@@ -21,26 +23,33 @@ app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.add_middleware(CSRFProtectMiddleware, csrf_secret=CSRF_SECRET_KEY)
 
 
-@app.route("/", methods=["GET", "POST"])
 @csrf_protect
+@app.route("/", methods=["GET", "POST"])
 async def index(request: Request):
     """GET|POST /: form handler"""
-    form = await UploadJobForm.from_formdata(request)
-    submit_jobs_form = SubmitJobsForm(request)
-    if await form.validate():
-        job = {
-            "source": form.source.data,
-            "experiment_type": form.experiment_type.data,
-            "acquisition_datetime": form.acquisition_datetime.data,
-            "modality": form.modality.data,
-        }
-        submit_jobs_form.jobs.append(job)
+    return templates.TemplateResponse("index.html", {"request": request, "json_data": ""})
 
-    return templates.TemplateResponse(
-        "jobs.html",
-        {
-            "request": request,
-            "form": form,
-            "submit_jobs_form": submit_jobs_form,
-        },
-    )
+
+@app.post("/upload/")
+async def upload_csv(request: Request, file: UploadFile = File(...)):
+    """POST /: csv handler"""
+    try:
+        content = await file.read()
+        data = content.decode("utf-8")
+
+        csv_data = []
+        csv_reader = csv.DictReader(io.StringIO(data))
+        for row in csv_reader:
+            csv_data.append(row)
+
+        return templates.TemplateResponse(
+            name="index.html",
+            context=({"request": request, "csv_data": csv_data}),
+        )
+
+    except Exception as e:
+        return JSONResponse(content={"error": f"Error processing CSV: {e}"}, status_code=400)
+
+app = FastAPI()
+app.routes.append(Route("/", index, methods=["GET"]))
+app.routes.append(Route("/upload/", upload_csv, methods=["POST"]))
