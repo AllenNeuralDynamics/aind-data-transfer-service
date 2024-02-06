eebd7c5599f36d8f215cbce8afce00070a7f0da3
Helen Lin <helen.lin@alleninstitute.org>
2024-02-05 17:47:09 -0800

    generate, save, and download job template xlsx

--- src/aind_data_transfer_service/server.py
+++ src/aind_data_transfer_service/server.py
@@ -1,5 +1,6 @@
 """Starts and Runs Starlette Service"""
 import csv
+import datetime
 import io
 import json
 import logging
@@ -7,12 +8,17 @@ import os
 from asyncio import sleep
 from pathlib import PurePosixPath
 
-import openpyxl
+from aind_data_schema.core.data_description import Modality, Platform
 from fastapi import Request
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
+from openpyxl import Workbook, load_workbook
+from openpyxl.styles import Font
+from openpyxl.utils import get_column_letter
+from openpyxl.worksheet.datavalidation import DataValidation
 from pydantic import SecretStr
 from starlette.applications import Starlette
+from starlette.responses import FileResponse
 from starlette.routing import Route
 
 from aind_data_transfer_service.configs.job_configs import (
@@ -47,6 +53,73 @@ templates = Jinja2Templates(directory=template_directory)
 # OPEN_DATA_AWS_ACCESS_KEY_ID
 
 OPEN_DATA_BUCKET_NAME = os.getenv("OPEN_DATA_BUCKET_NAME", "aind-open-data")
+JOB_TEMPLATE_FILENAME = os.getenv("TEMPLATE_FILE_NAME", "job_template.xlsx")
+JOB_TEMPLATE_FILEPATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), JOB_TEMPLATE_FILENAME)
+)
+JOB_TEMPLATE_HEADERS = [
+    "platform",
+    "acq_datetime",
+    "subject_id",
+    "s3_bucket",
+    "modality0",
+    "modality0.source",
+    "modality1",
+    "modality1.source",
+]
+JOB_TEMPLATE_JOBS = [
+    [
+        Platform.BEHAVIOR.abbreviation,
+        datetime.datetime(2023, 10, 4, 4, 0, 0),
+        "123456",
+        "aind-behavior-data",
+        Modality.BEHAVIOR_VIDEOS.abbreviation,
+        "/allen/aind/stage/fake/dir",
+        Modality.BEHAVIOR.abbreviation,
+        "/allen/aind/stage/fake/dir",
+    ],
+    [
+        Platform.SMARTSPIM.abbreviation,
+        datetime.datetime(2023, 3, 4, 16, 30, 0),
+        "654321",
+        "aind-open-data",
+        Modality.SPIM.abbreviation,
+        "/allen/aind/stage/fake/dir",
+    ],
+    [
+        Platform.ECEPHYS.abbreviation,
+        datetime.datetime(2023, 1, 30, 19, 1, 0),
+        "654321",
+        "aind-ephys-data",
+        Modality.ECEPHYS.abbreviation,
+        "/allen/aind/stage/fake/dir",
+        Modality.BEHAVIOR_VIDEOS.abbreviation,
+        "/allen/aind/stage/fake/dir",
+    ],
+]
+JOB_TEMPLATE_VALIDATORS = [
+    {
+        "name": "platform",
+        "options": [p().abbreviation for p in Platform._ALL],
+        "ranges": ["A2:A20"],
+    },
+    {
+        "name": "modality",
+        "options": [m().abbreviation for m in Modality._ALL],
+        "ranges": ["E2:E20", "G2:G20"],
+    },
+    {
+        "name": "s3_bucket",
+        "options": [
+            "aind-ephys-data",
+            "aind-ophys-data",
+            "aind-behavior-data",
+            "aind-private-data",
+            "aind-open-data",
+        ],
+        "ranges": ["D2:D20"],
+    },
+]
 
 
 async def validate_csv(request: Request):
@@ -63,7 +136,7 @@ async def validate_csv(request: Request):
                 # byte chars. Adding "utf-8-sig" should remove them.
                 data = content.decode("utf-8-sig")
             else:
-                xlsx_sheet = openpyxl.load_workbook(io.BytesIO(content)).active
+                xlsx_sheet = load_workbook(io.BytesIO(content)).active
                 csv_io = io.StringIO()
                 csv_writer = csv.writer(csv_io)
                 for r in xlsx_sheet.rows:
@@ -318,6 +391,52 @@ async def jobs(request: Request):
     )
 
 
+async def download_job_template(request: Request):
+    """Get job template as xlsx file download"""
+    if not os.path.isfile(JOB_TEMPLATE_FILEPATH):
+        await create_template_xlsx()
+    return FileResponse(
+        JOB_TEMPLATE_FILEPATH,
+        media_type="application/octet-stream",
+        filename=JOB_TEMPLATE_FILENAME,
+    )
+
+
+async def create_template_xlsx():
+    """Generate and save xlsx job template"""
+    # create job template
+    workbook = Workbook()
+    worksheet = workbook.active
+    worksheet.append(JOB_TEMPLATE_HEADERS)
+    for job in JOB_TEMPLATE_JOBS:
+        worksheet.append(job)
+    # add validation for select columns
+    for validator in JOB_TEMPLATE_VALIDATORS:
+        dv = DataValidation(
+            type="list",
+            formula1=f'"{(",").join(validator["options"])}"',
+            allow_blank=True,
+            showErrorMessage=True,
+            showInputMessage=True,
+        )
+        dv.prompt = f'Select a {validator["name"]} from the dropdown'
+        dv.error = f'Invalid {validator["name"]}.'
+        for r in validator["ranges"]:
+            dv.add(r)
+        worksheet.add_data_validation(dv)
+    # formatting
+    bold = Font(bold=True)
+    for header in worksheet["A1:H1"]:
+        for cell in header:
+            cell.font = bold
+            worksheet.column_dimensions[
+                get_column_letter(cell.column)
+            ].auto_size = True
+    # save file
+    workbook.save(JOB_TEMPLATE_FILEPATH)
+    return
+
+
 routes = [
     Route("/", endpoint=index, methods=["GET", "POST"]),
     Route("/api/validate_csv", endpoint=validate_csv, methods=["POST"]),
@@ -326,6 +445,11 @@ routes = [
     ),
     Route("/api/submit_hpc_jobs", endpoint=submit_hpc_jobs, methods=["POST"]),
     Route("/jobs", endpoint=jobs, methods=["GET"]),
+    Route(
+        "/api/download_job_template",
+        endpoint=download_job_template,
+        methods=["GET"],
+    ),
 ]
 
 app = Starlette(routes=routes)
