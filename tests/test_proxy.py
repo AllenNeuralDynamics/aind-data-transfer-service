5d47f24e86501bcd678cfbe9a1997a1870e937a2
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2025-08-19 15:57:08 -0700

    fix: runs linters

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,28 +1,12 @@
-"""Tests server module."""
+"""Tests server module and proxy method."""
 
-import asyncio
-import json
-import os
-import unittest
-from datetime import datetime, timedelta, timezone
-from io import BytesIO
-from pathlib import Path, PurePosixPath
 from unittest import IsolatedAsyncioTestCase
-from unittest.mock import AsyncMock, MagicMock, call, patch
+from unittest.mock import AsyncMock, patch
 
-from aind_data_schema_models.modalities import Modality
-from authlib.integrations.starlette_client import OAuthError
-from botocore.exceptions import ClientError
-from fastapi.responses import JSONResponse, StreamingResponse
-from fastapi.testclient import TestClient
 from httpx import AsyncClient, RequestError, Response
-from requests import Response
 from starlette.requests import Request
 
-from aind_data_transfer_service.configs.platforms_v1 import Platform
 from aind_data_transfer_service.server import proxy
-
-# from httpx import Response
 
 
 class TestProxyServer(IsolatedAsyncioTestCase):
@@ -37,8 +21,7 @@ class TestProxyServer(IsolatedAsyncioTestCase):
     ):
         """Tests proxy method."""
         mock_request_body.return_value = {"foo": "bar"}
-        mock_response = Response()
-        mock_response.status_code = 200
+        mock_response = Response(200)
         mock_async_client_request.return_value = mock_response
 
         response = await proxy(
