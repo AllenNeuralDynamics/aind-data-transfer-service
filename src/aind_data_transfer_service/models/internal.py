8e81e0e1befd5a73244b85b14919a6f6f0f7d93b
Helen Lin <helen.lin@alleninstitute.org>
2026-01-05 15:33:23 -0800

    fix: remove modality validator for job param name

--- src/aind_data_transfer_service/models/internal.py
+++ src/aind_data_transfer_service/models/internal.py
@@ -239,16 +239,6 @@ class JobParamInfo(BaseModel):
     modality: Optional[str]
     version: Optional[str] = Field(..., pattern=r"^(v1|v2)?$")
 
-    @field_validator("modality", mode="after")
-    def validate_modality(cls, v):
-        """Check that modality is one of aind-data-schema modalities"""
-        if v is not None and v not in JobParamInfo._MODALITIES_LIST:
-            raise ValueError(
-                "Invalid modality: modality must be one of "
-                f"{JobParamInfo._MODALITIES_LIST}"
-            )
-        return v
-
     @classmethod
     def from_aws_describe_parameter(
         cls,
